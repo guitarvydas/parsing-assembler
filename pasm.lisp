5a7d1fff34de1647d235c5941a2d7865cb66d8b3
Paul Tarvydas <paultarvydas@gmail.com>
2020-04-21 10:34:22 -0400

    add pasm.lisp

---
+++ pasm.lisp
@@ -0,0 +1,20 @@
+(in-package :parsing-assembler)
+
+(defun pasm-to-file (prototype-symbol infilename outfilename)
+  (let ((scanned-pasm (scanner:scanner (alexandria:read-file-into-string infilename))))
+    (let ((p (make-instance 'parser)))
+      (initially p scanned-pasm)
+      (<pasm> p)
+      (let ((pkg (package-name (find-package (symbol-package prototype-symbol)))))
+	(with-open-file (outf outfilename :direction :output :if-exists :supersede :if-does-not-exist :create)
+	  (let ((str (get-output-stream-string (pasm:output-string-stream p))))
+	    (format outf "(in-package :~a)~%~%" pkg)
+	    (write-string str outf)
+	    #+nil(with-input-from-string (ins str)
+		   (let ((sexp (read ins nil :eof)))
+		     (@:loop
+		       (@:exit-when (eq :eof sexp))
+		       (pprint sexp outf)
+		       (setf sexp (read ins nil :eof)))))))))))
+
+
