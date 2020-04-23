bdbc098f5f3a52c069be689cb2765712d0a9da24
Paul Tarvydas <paultarvydas@gmail.com>
2020-04-23 11:08:31 -0400

    made target package a parameter

--- pasm.lisp
+++ pasm.lisp
@@ -1,20 +1,19 @@
 (in-package :parsing-assembler)
 
-(defun pasm-to-file (prototype-symbol infilename outfilename)
+(defun pasm-to-file (package-name infilename outfilename)
   (let ((scanned-pasm (scanner:scanner (alexandria:read-file-into-string infilename))))
     (let ((p (make-instance 'parser)))
       (initially p scanned-pasm)
       (<pasm> p)
-      (let ((pkg (package-name (find-package (symbol-package prototype-symbol)))))
-	(with-open-file (outf outfilename :direction :output :if-exists :supersede :if-does-not-exist :create)
-	  (let ((str (get-output-stream-string (pasm:output-string-stream p))))
-	    (format outf "(in-package :~a)~%~%" pkg)
-	    (write-string str outf)
-	    #+nil(with-input-from-string (ins str)
-		   (let ((sexp (read ins nil :eof)))
-		     (@:loop
-		       (@:exit-when (eq :eof sexp))
-		       (pprint sexp outf)
-		       (setf sexp (read ins nil :eof)))))))))))
+      (with-open-file (outf outfilename :direction :output :if-exists :supersede :if-does-not-exist :create)
+	(let ((str (get-output-stream-string (pasm:output-string-stream p))))
+	  (format outf "(in-package :~s)~%~%" package-name)
+	  (write-string str outf)
+	  #+nil(with-input-from-string (ins str)
+		 (let ((sexp (read ins nil :eof)))
+		   (@:loop
+		     (@:exit-when (eq :eof sexp))
+		     (pprint sexp outf)
+		     (setf sexp (read ins nil :eof))))))))))
 
 
